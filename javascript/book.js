d6317c982efd8469dd2eafc8c86a7dbd26131f08
LoboArkano <pipiolo_mananes@hotmail.com>
2020-08-05 15:10:15 -0500

    Add style to new book button

--- javascript/book.js
+++ javascript/book.js
@@ -3,20 +3,23 @@ let myBooksList = [
     image_url: 'https://a.wattpad.com/cover/22127925-288-k860552.jpg',
     title: 'Harry Potter Chapter 1',
     author: 'Jose Roberto',
-    pages: 535
+    pages: 535,
+    read: false,
   },
   {
     image_url: 'https://images-na.ssl-images-amazon.com/images/I/81s5gr8znaL.jpg',
     title: 'Alchimist',
     author: 'Zakariae El Mejdki',
-    pages: 230
+    pages: 230,
+    read: true,
   },
   {
     image_url: 'https://miro.medium.com/max/480/1*E6aoQIUVWcJbDbXulkLpkg.jpeg',
     title: 'Rich Dad Poor Dad',
     author: 'Zakariae Roberto',
-    pages: 354
-  }
+    pages: 354,
+    read: false,
+  },
 ];
 
 function Book() {
@@ -34,6 +37,11 @@ function render () {
       <div class="item align-center d-flex">
         <img class="book-image" src="${book.image_url}">
         <div class="book-info">
+          <p class="d-flex align-center status ${book.read ? 'green' : 'red'}">
+            <span class="circle ${book.read ? 'circle-green' : 'circle-red'}">
+            </span>
+            ${book.read ? 'Already read' : 'Not read yet'}
+          </p>
           <p class="book-title">${book.title}</p>
           <p class="book-author">author: ${book.author}</p>
           <p class="book-pages">pages: ${book.pages}</p>
