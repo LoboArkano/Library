6bf439bf99496fb3eb1014596dfc1ae83f53c590
LoboArkano <pipiolo_mananes@hotmail.com>
2020-08-11 09:52:30 -0500

    Move firebase configuration

---
+++ javascript/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDE3KLpPwM6bGgFYZRIelV_ymVUNibH6Mg",
+  authDomain: "library-d0f55.firebaseapp.com",
+  databaseURL: "https://library-d0f55.firebaseio.com",
+  projectId: "library-d0f55",
+  storageBucket: "library-d0f55.appspot.com",
+  messagingSenderId: "905261953149",
+  appId: "1:905261953149:web:21bbe2e3f8eee115c10114"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
